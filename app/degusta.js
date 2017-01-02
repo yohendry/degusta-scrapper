ff328d69076ab7f1b99304fa202caf8ee0c94f7a
Yohendry Hurtado <yohendry.hurtado@gmail.com>
2017-01-01 23:39:40 -0500

    get top restaurants based on food, service or ambience - got info from an especific restaurant

---
+++ app/degusta.js
@@ -0,0 +1,151 @@
+'use strict';
+
+const scrapeIt = require("scrape-it");
+const _ = require("lodash");
+
+class DegustaAPI {
+  constructor(url) {
+    this.url = url;
+  }
+
+  test(callback) {
+    return scrapeIt(this.url, {
+        title: ".header h1",
+        desc: ".header h2",
+        avatar: {
+          selector: ".header img",
+          attr: "src"
+        }
+      },
+      callback);
+  }
+
+  goToRestaurant(params, callback) {
+    var url;
+    if (params.url) {
+      if (_.isString(this.url)) {
+        url = `${this.url}${params.url}`;
+      } else {
+        url = this.url;
+        url.url = `${url.url}${params.url}`;
+      }
+    }
+    return scrapeIt(url, {
+        title: "h1#restaurante",
+        phone: {
+          selector: '#main .btn-show-telefono',
+          attr: 'data-phone'
+        },
+        image: {
+          selector: ".bxslider li img",
+          attr: "src"
+        },
+        direction: '.direccion-holder address.adr span',
+        hours: '.horario-holder span.horario',
+        hasOnlineBooking: {
+          selector: '.new-widget-reserva',
+          convert: value => {
+            return value.trim().length > 0 ? true : false;
+          }
+        },
+        ratings: {
+          listItem: '.rating-list .rating',
+          data: {
+            name: {
+              selector: '.rating-title',
+              convert: value => {
+                value = value.replace(/([^a-z0-9áéíóúñü_-\s\.,]|[\t\n\f\r\v\0])/gim, "");
+                return value;
+              }
+            },
+            value: {
+              selector: '.rating-value > strong',
+              convert: value => {
+                let index = value.indexOf('/');
+                if (index >= 0) {
+                  return value.substring(0, value.indexOf('/'));
+                } else {
+                  return value;
+                }
+
+              }
+            }
+          }
+        }
+      },
+      callback);
+  }
+
+  topRestaurants(params, callback) {
+    var tab = "tab201";
+    var priceIndex = 0;
+
+    switch (params.price) {
+      case "expensive":
+        priceIndex = 0;
+        break;
+      case "pricy":
+        priceIndex = 0;
+        break;
+      case "mid higth":
+        priceIndex = 1;
+        break;
+      case "mid low":
+        priceIndex = 2;
+        break;
+      case "cheap":
+        priceIndex = 3;
+        break;
+      case "cheap":
+        priceIndex = 3;
+        break;
+      default:
+        priceIndex = 0;
+        break;
+    }
+
+
+    switch (params.category) {
+      case "food":
+        tab = "tab201";
+        break;
+      case "service":
+        tab = "tab202";
+        break;
+      case "ambience":
+        tab = "tab203";
+        break;
+      default:
+        tab = "tab201";
+        break;
+    }
+
+    return scrapeIt(this.url, {
+        categories: {
+          listItem: `#${tab} .block`,
+          data: {
+            title: 'a h3',
+            restaurants: {
+              listItem: 'ol > li',
+              data: {
+                name: 'a',
+                url: {
+                  selector: 'a',
+                  attr: "href"
+                }
+              }
+            }
+          }
+        }
+      },
+      function(err, data) {
+        if (err) {
+          callback(err, {});
+        } else {
+          callback(err, data.categories[priceIndex]);
+        }
+      });
+  }
+}
+
+module.exports = DegustaAPI;
---
+++ tests/api/test.js
@@ -0,0 +1,75 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+var DegustaAPI = require('./../../app/degusta');
+
+describe('Degusta', function() {
+  this.timeout(10000);
+
+  it('scrapper should works', function(done) {
+    var degusta = new DegustaAPI("http://ionicabizau.net");
+    degusta.test(function(err, data) {
+      if (err) {
+        done(err);
+      } else {
+        expect(data.title).to.equal('Ionică Bizău');
+        done();
+      }
+    });
+  });
+
+  it('can get top restaurants', function(done) {
+    var degusta = new DegustaAPI({
+      url: 'https://www.degustapanama.com/',
+      headers: {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
+      }
+    });
+
+    degusta.topRestaurants({
+      category: 'food',
+      price: 'cheap'
+    }, function(err, data) {
+      if (err) {
+        done(err);
+      } else {
+        expect(data.restaurants.length).to.equal(4);
+        done();
+      }
+    });
+  });
+
+  it('can get restaurant data', function(done) {
+    var degusta = new DegustaAPI({
+      url: 'https://www.degustapanama.com/',
+      headers: {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
+      }
+    });
+
+    degusta.goToRestaurant({
+      url: '/restaurante/gelato-y-co_101955.html'
+    }, function(err, data) {
+      if (err) {
+        done(err);
+      } else {
+        expect(data.title).to.equal('Gelato & Co');
+        expect(data.ratings.length).to.equal(5);
+        expect(data.hasOnlineBooking).to.equal(false);
+        done();
+      }
+    });
+
+    degusta.goToRestaurant({
+      url: '/restaurante/vinoteca_340.html'
+    }, function(err, data) {
+      if (err) {
+        done(err);
+      } else {
+        expect(data.title).to.equal('Vinoteca');
+        expect(data.ratings.length).to.equal(5);
+        expect(data.hasOnlineBooking).to.equal(true);
+        done();
+      }
+    });
+  });
+});
